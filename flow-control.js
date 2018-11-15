131b997909e0faccbd93daa2a300286189774d49
Patrick Rafferty <paddyraff@gmail.com>
2018-11-15 15:51:47 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,19 @@
-function basicTeenager(age) {
+
+function basicTeenager(age)
+
+{
+return ("You are a teenager!")
+
+}
+basicTeenager(12)
+{
 
 }
 
 function teenager(age) {
-
+return ("You are a kid")
 }
-
+teenager(12)
 function ageChecker(age) {
 
 }
